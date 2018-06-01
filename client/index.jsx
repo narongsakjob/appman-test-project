ec86e5f94960127681807a68c9ca8cbf2d302c3c
Narongsak <narongsak.chobsri@gmail.com>
2018-06-01 18:20:02 +0700

    Initial project

---
+++ client/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+    <h1> Hello Landing asdsadsasddsaaaaaaada</h1>
+   {/* 
+    This a random image I made, feel free to skip this in your code
+    If you want to add your own image you can add it to the server/public/img folder 
+    */}
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('.root'));
