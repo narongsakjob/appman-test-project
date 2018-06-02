48713357a6145b7da36906c29ed7d5daca4b03fb
Narongsak <narongsak.chobsri@gmail.com>
2018-06-02 17:25:45 +0700

    Add concurrently for run nodemon and watch

--- client/index.jsx
+++ client/index.jsx
@@ -4,11 +4,7 @@ import ReactDOM from 'react-dom';
 const App = () => {
   return (
     <div>
-    <h1> Hello Landing asdsadsasddsaaaaaaada</h1>
-   {/* 
-    This a random image I made, feel free to skip this in your code
-    If you want to add your own image you can add it to the server/public/img folder 
-    */}
+      <h1> Hello Landing eiei</h1>
     </div>
   );
 };
